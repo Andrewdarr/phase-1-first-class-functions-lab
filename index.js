9971d1c0366be65fe240532cff511e218bfa84a8
Andrew Darr <kildar07@gmail.com>
2022-08-12 10:29:41 -0500

    commit tree ac7bb18876fdfbc56e05ec13efb16d6a1e9927bc

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = () => {
+    let returnFirstTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+}
+console.log(returnFirstTwoDrivers)
+returnFirstTwoDrivers()
+console.log(returnFirstTwoDrivers)
+returnFirstTwoDrivers.slice(2,4)
+console.log(returnFirstTwoDrivers)
